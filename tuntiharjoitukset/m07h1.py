20864108fb3af77b01cd0a9091226a7fb84fa973
ptpar <pinja.t.parviainen@gmail.com>
2025-10-22 14:31:41 +0300

    Delete tuntiharjoitukset directory

--- tuntiharjoitukset/m07h1.py
+++
@@ -1,6 +0,0 @@
-numbers = []
-luku = int(input("Syötä luku. Luku 0 keskeyttää ohjelman. "))
-while luku != 0:
-    numbers.append(luku)
-    luku = int(input("Syötä luku. Luku 0 keskeyttää ohjelman. "))
-print(list(set(numbers)))
